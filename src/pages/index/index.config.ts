318b1aa7d5ca81e53090b9666b3c49f7290a86df
HuangYaobin <178746489@qq.com>
2022-12-02 14:18:49 +0800

    create

---
+++ src/pages/index/index.config.ts
@@ -0,0 +1,6 @@
+export default definePageConfig({
+ navigationBarTitleText: '首页',
+ usingComponents: {
+  'mp-html': '../../components/mp-html/index'
+ }
+})
---
+++ src/pages/index/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const content = `<div>123</div>`
+</script>
+<template>
+  <view class="index">
+    <mp-html :content="content"></mp-html>
+  </view>
+</template>
+
